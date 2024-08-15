f0bdc3ee143848addb168d041044be51bb0cddf9
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-08-15 14:51:50 +0200

    Add PHP importer script

--- tests/E2E/setup/wp-env.config.js
+++ tests/E2E/setup/wp-env.config.js
@@ -1,27 +1,31 @@
 // This creates a .wp-env.json file using the .env file in the root of the project.
 
-const fs = require( 'fs' );
-const path = require( 'path' );
+const fs = require('fs');
+const path = require('path');
 
-require( 'dotenv' ).config( { path: `${ process.env.INIT_CWD }/.env` } );
+require('dotenv').config({ path: `${process.env.INIT_CWD}/.env` });
 
 const wpEnvConfig = {
-	phpVersion: '7.2',
-	plugins: [ '../../..', process.env.GRAVITY_FORMS_FOLDER || '/tmp/gravityforms' ],
-	port: parseInt( process.env.WP_ENV_PORT, 10 ) || 8888,
-	config: {
-		WP_DEBUG: true,
-		WP_DEBUG_LOG: true,
-		WP_DEBUG_DISPLAY: true,
-		SAVEQUERIES: true,
-		SCRIPT_DEBUG: true,
-	},
+  phpVersion: '7.2',
+  plugins: [
+    '../../..',
+    process.env.GRAVITY_FORMS_FOLDER || '/tmp/gravityforms',
+    process.env.GF_FORM_IMPORTER || '/tmp/gf-forms-importer',
+  ],
+  port: parseInt(process.env.WP_ENV_PORT, 10) || 8888,
+  config: {
+    WP_DEBUG: true,
+    WP_DEBUG_LOG: true,
+    WP_DEBUG_DISPLAY: true,
+    SAVEQUERIES: true,
+    SCRIPT_DEBUG: true,
+  },
 };
 
 fs.writeFileSync(
-	path.join( __dirname, '.wp-env.json' ),
+  path.join(__dirname, '.wp-env.json'),
 
-	JSON.stringify( wpEnvConfig, null, 2 )
+  JSON.stringify(wpEnvConfig, null, 2)
 );
 
-console.log( 'Generated .wp-env.json' );
+console.log('Generated .wp-env.json');
