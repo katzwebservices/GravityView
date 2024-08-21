d2fc570cf8940390dcbc93e87d0468e7fa9e9829
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-08-21 16:54:07 +0200

    Add layout tests

---
+++ tests/E2E/tests/layout/TemplateSelection.spec.js
@@ -0,0 +1,85 @@
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { selectGravityFormByTitle } from '../../helpers/test-helpers';
+
+require('dotenv').config({ path: `${process.env.INIT_CWD}/.env` });
+
+const url = process.env.URL;
+
+test.describe('GravityView Template Selection', () => {
+  const templates = [
+    {
+      name: 'Table',
+      slug: 'default_table',
+      selector: '.gv-view-types-module:has(h5:text("Table"))',
+    },
+    {
+      name: 'List',
+      slug: 'default_list',
+      selector: '.gv-view-types-module:has(h5:text("List"))',
+    },
+    {
+      name: 'DataTables Table',
+      slug: 'datatables_table',
+      selector: '.gv-view-types-module:has(h5:text("DataTables Table"))',
+    },
+  ];
+
+  const form = {
+    filename: 'simple',
+    title: 'A Simple Form',
+  };
+
+  for (const template of templates) {
+    test(`Verify GravityView template: ${template.name}`, async ({ page }) => {
+      await page.goto(`${url}/wp-admin/edit.php?post_type=gravityview`);
+      await page.waitForSelector('text=New View', { state: 'visible' });
+
+      await page.click('text=New View');
+
+      await selectGravityFormByTitle(page, form.title);
+
+      await page.fill('#title', `Test View - ${template.name}`);
+
+      page.waitForSelector('#gravityview_select_template', {
+        state: 'visible',
+      });
+
+      await page.waitForSelector('.gv-view-types-module', {
+        state: 'visible',
+      });
+
+      const templateSelector = await page.$(template.selector);
+
+      //TODO: Check if is placeholder
+      const placeholder = '.gv-view-template-placeholder';
+
+      
+      const selectButtonLocator = page.locator(
+        `a.gv_select_template[data-templateid="${template.slug}"]`
+      );
+      await templateSelector.hover();
+      await page.dispatchEvent(template.selector, 'mouseenter');
+      await selectButtonLocator.waitFor({ state: 'visible' });
+      await page.screenshot({ path: 'test-screenshot-2.png' });
+      await selectButtonLocator.click();
+
+      await Promise.all([
+        page.click('#publish'),
+        page.waitForURL(/\/wp-admin\/post\.php\?post=\d+&action=edit/),
+      ]);
+
+      await page.waitForSelector('.notice-success');
+      const successMessage = await page.textContent('.notice-success');
+      expect(successMessage).toContain('View published');
+
+      const viewUrl = await page.$eval('#sample-permalink', (el) => el.href);
+      await page.goto(viewUrl);
+
+      // TODO: Add actual verification logic
+      await page.waitForURL(viewUrl);
+      const entryExists = await page.locator('.gv-grid');
+      expect(entryExists).toBeTruthy();
+
+    });
+  }
+});
