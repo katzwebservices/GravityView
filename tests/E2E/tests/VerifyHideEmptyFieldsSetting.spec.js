85e3d227562dcb95bf06ac4d76af9e48e338f837
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-09-02 10:05:55 +0200

    Add test to verify hide empty fields setting

---
+++ tests/E2E/tests/VerifyHideEmptyFieldsSetting.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { checkViewOnFrontEnd, createView, gotoAndEnsureLoggedIn, publishView, templates } from '../helpers/test-helpers';
+
+test('Verify Hide Empty Fields Setting', async ({ page }, testInfo) => {
+    await gotoAndEnsureLoggedIn(page, testInfo);
+
+    await createView(page, { formTitle: 'Has Empty Field', viewName: 'Hide Empty Fields Setting Test', template: templates[0] });
+    await publishView(page);
+    await checkViewOnFrontEnd(page);
+
+    const thead = await page.locator('table.gv-table-view thead');
+    const thElements = await thead.locator('th');
+    await expect(thElements).toHaveCount(1);
+});
