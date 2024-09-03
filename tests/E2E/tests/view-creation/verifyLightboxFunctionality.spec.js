a737402b7abd995d3b6babccaaf62b299c46f88e
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-09-03 17:14:14 +0200

    Simplify Lightbox functionality test

--- tests/E2E/tests/view-creation/verifyLightboxFunctionality.spec.js
+++ tests/E2E/tests/view-creation/verifyLightboxFunctionality.spec.js
@@ -1,49 +1,14 @@
 import { test, expect } from '@playwright/test';
-import { gotoAndEnsureLoggedIn, selectGravityFormByTitle } from '../../helpers/test-helpers';
+import { checkViewOnFrontEnd, createView, gotoAndEnsureLoggedIn, publishView, templates } from '../../helpers/test-helpers';
 
 test('Verify Lightbox Functionality for Images', async ({ page }, testInfo) => {
     await gotoAndEnsureLoggedIn(page, testInfo);
-
-    await page.getByText('New View', { exact: true }).click();
-
-    await page.getByLabel('Enter View name here').click();
-    await page.getByLabel('Enter View name here').fill('Image Lightbox Test');
-
-    const form = {
-        filename: 'complete',
-        title: 'Complete',
-    };
-
-    await selectGravityFormByTitle(page, form.title);
-
-    await page.waitForSelector('#gravityview_select_template', { state: 'visible' });
-    await page.waitForSelector('.gv-view-types-module', { state: 'visible' });
-
-    const tableTemplateSelector = await page.$('div.gv-view-types-module:has(a.gv_select_template[href="#gv_select_template"][data-templateid="default_table"])');
-    if (!tableTemplateSelector) {
-        throw new Error('Table template not found.');
-    }
-    await tableTemplateSelector.hover();
-    const selectButtonLocator = page.locator('a.gv_select_template[data-templateid="default_table"]');
-    await selectButtonLocator.waitFor({ state: 'visible' });
-    await selectButtonLocator.click();
-
+    await createView(page, { formTitle: 'Complete', viewName: 'Image Lightbox Test', template: templates[0] });
     await page.getByLabel('Show only approved entries').setChecked(false);
-
-    await Promise.all([
-        page.click('#publish'),
-        page.waitForURL(/\/wp-admin\/post\.php\?post=\d+&action=edit/),
-    ]);
-    await page.waitForSelector('.notice-success');
-    const successMessage = await page.textContent('.notice-success');
-    expect(successMessage).toContain('View published');
-
-    const viewUrl = await page.$eval('#sample-permalink', (el) => el.href);
-    await page.goto(viewUrl);
-
+    await publishView(page);
+    await checkViewOnFrontEnd(page);
     const imageLocator = page.locator('.gv-container .gravityview-fancybox').first();
     await expect(imageLocator).toBeVisible();
-    
     await imageLocator.click();
     await expect(page.locator('.fancybox__container')).toBeVisible();
 });
