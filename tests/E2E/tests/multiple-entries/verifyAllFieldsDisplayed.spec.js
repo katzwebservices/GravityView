631a97f4f0b29b65908ab1f3fe3838e64e12ee10
Mwale Kalenga <mwale.kalenga@prosites.space>
2024-10-15 17:06:45 +0200

    Improve test to verify all fields display

--- tests/E2E/tests/multiple-entries/verifyAllFieldsDisplayed.spec.js
+++ tests/E2E/tests/multiple-entries/verifyAllFieldsDisplayed.spec.js
@@ -13,12 +13,12 @@ test('Verify All Fields Are Displayed Correctly', async ({ page }, testInfo) => 
     await expect(page.getByText('alice@example.com')).toBeVisible();
     await expect(page.getByText('bob@example.com')).toBeVisible();
 
-    await expect(page.getByText('35')).toBeVisible();
-    await expect(page.getByText('45')).toBeVisible();
+    await expect(page.getByText('35', { exact: true })).toBeVisible();
+    await expect(page.getByText('45', { exact: true })).toBeVisible();
 
-    await expect(page.getByText('06/15/1994')).toBeVisible();
-    await expect(page.getByText('08/22/1979')).toBeVisible();
+    await expect(page.getByText('06/15/1994', { exact: true })).toBeVisible();
+    await expect(page.getByText('08/22/1979', { exact: true })).toBeVisible();
 
-    await expect(page.getByText('Red')).toBeVisible();
-    await expect(page.getByText('Blue')).toBeVisible();
+    await expect(page.getByText('Red', { exact: true })).toBeVisible();
+    await expect(page.getByText('Blue', { exact: true })).toBeVisible();
 });
