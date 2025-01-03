4de16c368efdad4109ba842461868cee12af1bcf
Mwale Kalenga <mwale.kalenga@prosites.space>
2025-01-03 15:28:24 +0200

    Add testInfo to custom JS test

--- tests/E2E/tests/custom-code/customJS.spec.js
+++ tests/E2E/tests/custom-code/customJS.spec.js
@@ -6,7 +6,7 @@ import { checkViewOnFrontEnd, createView, gotoAndEnsureLoggedIn, publishView, te
  */
 test('Verify Custom JS', async ({ page }, testInfo) => {
     await gotoAndEnsureLoggedIn(page, testInfo);
-    await createView(page, { formTitle: 'Event Registration', viewName: 'Verify Custom JS Test', template: templates[0] });
+    await createView(page, { formTitle: 'Event Registration', viewName: 'Verify Custom JS Test', template: templates[0] }, testInfo);
     await page.locator('#gravityview_settings div').getByRole('link', { name: 'Custom Code' }).click();
     await page.fill('#gravityview_advanced tbody tr:nth-of-type(2) .CodeMirror-activeline', 'document.body.setAttribute("data-test", "custom-js-applied");');
     await publishView(page);
