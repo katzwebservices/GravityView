c2af884653f44bc4101709b5fcbd6f839779df52
Mwale Kalenga <mwale.kalenga@prosites.space>
2025-01-24 19:41:44 +0200

    Use test.step for permissions tests

--- tests/E2E/tests/permissions/disallowExport.spec.js
+++ tests/E2E/tests/permissions/disallowExport.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 import {
 	checkViewOnFrontEnd,
 	clickDownloadButton,
@@ -7,26 +7,42 @@ import {
 	gotoAndEnsureLoggedIn,
 	publishView,
 	templates,
-} from '../../helpers/test-helpers';
+} from "../../helpers/test-helpers";
 
 /**
  * Confirms entry downloads don’t work when 'Allow Export' setting is disabled.
  */
-test('Verify Disallow Export', async ({ page }, testInfo) => {
+test("Verify Disallow Export", async ({ page }, testInfo) => {
 	let noDownload = true;
-	page.on('download', (download) => {
-		noDownload = false;
+
+	await test.step("Log in and navigate to the appropriate page", async () => {
+		await gotoAndEnsureLoggedIn(page, testInfo);
 	});
-	await gotoAndEnsureLoggedIn(page, testInfo);
-	await createView(page, {
-		formTitle: 'Favorite Color',
-		viewName: 'Verify Disallow Export Test',
-		template: templates[0],
+
+	await test.step("Create a new View with the Favorite Color form", async () => {
+		await createView(page, {
+			formTitle: "Favorite Color",
+			viewName: "Verify Disallow Export Test",
+			template: templates[0],
+		});
 	});
-	await publishView(page);
+
+	await test.step("Publish the View with the Allow Export setting disabled", async () => {
+		await publishView(page);
+	});
+
 	const viewUrl = await getViewUrl(page);
 	const downloadUrl = `${viewUrl}csv/`;
-	await checkViewOnFrontEnd(page);
-	await clickDownloadButton(page, downloadUrl);
-	expect(noDownload).toBe(true);
+
+	await test.step("Attempt to download entries with export disabled", async () => {
+		await checkViewOnFrontEnd(page);
+		page.on("download", () => {
+			noDownload = false;
+		});
+		await clickDownloadButton(page, downloadUrl);
+	});
+
+	await test.step("Verify no download happens when Allow Export is disabled", async () => {
+		expect(noDownload).toBe(true);
+	});
 });
--- tests/E2E/tests/permissions/enableEnhancedSecurity.spec.js
+++ tests/E2E/tests/permissions/enableEnhancedSecurity.spec.js
@@ -1,31 +1,46 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 import {
 	createPageWithShortcode,
 	createView,
 	gotoAndEnsureLoggedIn,
 	publishView,
 	templates,
-} from '../../helpers/test-helpers';
+} from "../../helpers/test-helpers";
 
 /**
  * Ensures a View without a valid secret shows an error message.
  */
-test('Verify Ehanced Security', async ({ page }, testInfo) => {
-	await gotoAndEnsureLoggedIn(page, testInfo);
-	await createView(page, {
-		formTitle: 'Favorite Color',
-		viewName: 'Verify Ehanced Security Test',
-		template: templates[0],
+test("Verify Enhanced Security", async ({ page }, testInfo) => {
+	await test.step("Log in and navigate to the appropriate page", async () => {
+		await gotoAndEnsureLoggedIn(page, testInfo);
 	});
-	await publishView(page);
+
+	await test.step("Create a View with the Favorite Color form", async () => {
+		await createView(page, {
+			formTitle: "Favorite Color",
+			viewName: "Verify Enhanced Security Test",
+			template: templates[0],
+		});
+	});
+
+	await test.step("Publish the View", async () => {
+		await publishView(page);
+	});
+
 	const currentUrl = page.url();
 	const params = new URLSearchParams(new URL(currentUrl).search);
-	const viewId = params.get('post');
-	const url = await createPageWithShortcode(page, {
-		shortcode: `gravityview id="${viewId}"`,
-		title: 'Verify Ehanced Security Test Page',
+	const viewId = params.get("post");
+
+	await test.step("Create a page with a shortcode for the View", async () => {
+		const url = await createPageWithShortcode(page, {
+			shortcode: `gravityview id="${viewId}"`,
+			title: "Verify Enhanced Security Test Page",
+		});
+		await page.goto(url);
 	});
-	await page.goto(url);
-	const errorMessage = page.locator('text=Invalid View secret provided');
-	await expect(errorMessage).toBeVisible();
+
+	await test.step("Check for the error message when no valid secret is provided", async () => {
+		const errorMessage = page.locator("text=Invalid View secret provided");
+		await expect(errorMessage).toBeVisible();
+	});
 });
